be6dc81ed039c4a92b000d38057c1042ca19c12c
Ernesto Canquiz <fdsoil123@gmail.com>
2023-04-10 22:09:25 -0400

    feat: Creating DB & GetData

--- app.ts
+++ app.ts
@@ -1,8 +1,84 @@
 import fs from 'fs'
 import ejs from 'ejs'
-import loadData from './modules/loadData'
+import {
+  getProduct,
+  getNamesList,
+  getSchemasAndTablesOfBD,
+  getSchemasOfBD,
+  getTableDetailForeignKeysAssoc,
+  getTableDetailOfMaster,
+  getTableForeignKeysAssocMasterDetail,
+  getTableMasterForeignKeysAssoc,
+  getTablePrimaryKey,
+  getTableStructure,
+  getTableUniqueConstraint,
+  getTablesOfBD
+} from './modules/GetData'
 
-loadData('products', 'presentations')
+const process = async () => {
+    const product = (await getProduct()).rows
+    console.log('namesList:', product, '\n')
+
+    const namesList = (await getNamesList()).rows
+    console.log('namesList:', namesList, '\n')
+
+    const schemasAndTablesOfBD = (await getSchemasAndTablesOfBD()).rows
+    console.log('schemasAndTablesOfBD:', schemasAndTablesOfBD, '\n')
+
+    const schemasOfBD = (await getSchemasOfBD()).rows
+    console.log('schemasOfBD:', schemasOfBD, '\n')
+
+    const tableDetailForeignKeysAssoc = (await getTableDetailForeignKeysAssoc({
+        schema: 'public',
+        tableMaster: 'products',
+        tableDetail: 'presentations'
+    })).rows
+    console.log('tableDetailForeignKeysAssocs: ', tableDetailForeignKeysAssoc, '\n')
+
+    const tableDetailOfMaster = (await getTableDetailOfMaster({
+      schema: 'public',
+      tableMaster: 'products'
+    })).rows
+    console.log('tableDetailOfMaster: ', tableDetailOfMaster, '\n')
+
+    const tableForeignKeysAssocMasterDetail = (await getTableForeignKeysAssocMasterDetail({
+      schema: 'public',
+      tableMaster: 'products',
+      tableDetail: 'presentations'
+  })).rows
+    console.log('tableForeignKeysAssocMasterDetail: ', tableForeignKeysAssocMasterDetail, '\n')
+
+    const tableMasterForeignKeysAssoc = (await getTableMasterForeignKeysAssoc({
+      schema: 'public',
+      tableMaster: 'products'
+    })).rows
+    console.log('tableMasterForeignKeysAssoc: ', tableMasterForeignKeysAssoc, '\n')
+    
+    const tablePrimaryKey = (await getTablePrimaryKey({
+        schema: 'public',
+        tableMaster: 'products'
+    })).rows
+    console.log('tablePrimaryKey: ', tablePrimaryKey, '\n')
+
+    const tableStructure = (await getTableStructure({
+      schema: 'public',
+      tableMaster: 'products'
+    })).rows
+    console.log('tableStructure: ', tableStructure, '\n')
+
+    const tableUniqueConstraint = (await getTableUniqueConstraint({
+      schema: 'public',
+      tableMaster: 'products'
+    })).rows
+    console.log('tableUniqueConstraint: ', tableUniqueConstraint, '\n')
+
+    const tablesOfBD = (await getTablesOfBD({ schema: 'public' })).rows
+    console.log('tablesOfBD: ', tablesOfBD, '\n')   
+    
+}
+
+process()
+
 
 let people = ['geddy', 'neil', 'alex'];
 let content = ejs.render('this-is-a-string [ <%= people.join(", "); %> ] this-is-another-string', {people});

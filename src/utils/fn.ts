77a95d7fb217ec8b65802495d5622c099b6289e4
Ernesto Canquiz <caribestic123@gmail.com>
2023-09-22 09:45:13 -0400

    refactor: Utility validator functions

--- src/utils/fn.ts
+++ src/utils/fn.ts
@@ -1,17 +1,26 @@
 import { uCamelCase, camelCase } from './nomenclature';
 import { singular } from './grammaticalNumber'
 
+const addCommaToArr = ( arr: any[], index: number, less = 0 ) =>
+  index < arr.length - less ? ', ' : ''
+
+const noId = (column_name: string): boolean => 
+  column_name !=='id'
+
+const excludeFields = (column_name: string): boolean =>
+  (!['created_at', 'updated_at', 'deleted_at'].includes(column_name))
+
+const noIdAndExcludeFields = (column_name: string): boolean =>
+  noId(column_name) && excludeFields(column_name)
+
 export default {
-  addCommaToArr: (
-    arr: any[], index: number, less = 0
-  ) => index < arr.length - less ? ', ' : '',
+  addCommaToArr,
   camelCase,
   singular,
   uCamelCase,
-  test1: (n:any)=> n,
-  vn: {
-    noId: (column_name: string): boolean => column_name !=='id',
-    excludeFields: (column_name: string): boolean => (!['created_at', 'updated_at', 'deleted_at'].includes(column_name)),
-    noIdAndExcludeFields: (column_name: string): boolean => column_name !=='id' && !['created_at', 'updated_at', 'deleted_at'].includes(column_name)
+  v: {
+    excludeFields,
+    noId,
+    noIdAndExcludeFields
   }
 }

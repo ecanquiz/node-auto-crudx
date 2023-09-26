4f1e5b8edd293f7dd246b05aac2c6766321dfae3
Ernesto Canquiz <caribestic123@gmail.com>
2023-09-26 11:10:16 -0400

    refactor: Reorder names on the stack

--- src/modules/renderings/laravel.ts
+++ src/modules/renderings/laravel.ts
@@ -3,117 +3,115 @@ import { uCamelCase } from '@utils/nomenclature'
 import type { ParamsAll, Rendering } from '@customTypes/utilsRendering'
 
 export default {
-
-  controller: (params: ParamsAll): Rendering => ({
-    template: './src/templates/laravel/controller',
-    outputPath: `Http/Controllers`,
-    outputFile: `${uCamelCase(singular(params.tableMaster))}Controller.php`,
-    params
-  }),
-
-  controllerDetail: (params: ParamsAll): Rendering => ({
+  detailController: (params: ParamsAll): Rendering => ({
     template: './src/templates/laravel/controllerDetail',
     outputPath: `Http/Controllers`,
     outputFile: `${uCamelCase(singular(params.tableDetailCurrent!.tableName))}Controller.php`,
     params
   }),
 
-  model: (params: ParamsAll): Rendering => ({
-    template: './src/templates/laravel/model',
-    outputPath: `Entities`,
-    outputFile: `${uCamelCase(singular(params.tableMaster))}.php`,
-    params
-  }),
-
-  modelDetail: (params: ParamsAll): Rendering => ({
+  detailModel: (params: ParamsAll): Rendering => ({
     template: './src/templates/laravel/modelDetail',
     outputPath: `Entities`,
     outputFile: `${uCamelCase(singular(params.tableDetailCurrent!.tableName))}.php`,
     params
   }),
 
-  requestStore: (params: ParamsAll): Rendering => ({
-    template: './src/templates/laravel/requestStore',
-    outputPath: `Http/Requests/${uCamelCase(singular(params.tableMaster))}`,
-    outputFile: `Store${uCamelCase(singular(params.tableMaster))}Request.php`,
-    params
-  }),
-
-  requestUpdate: (params: ParamsAll): Rendering => ({
-    template: './src/templates/laravel/requestUpdate',
-    outputPath: `Http/Requests/${uCamelCase(singular(params.tableMaster))}`,
-    outputFile: `Update${uCamelCase(singular(params.tableMaster))}Request.php`,
-    params
-  }),
-
-  resource: (params: ParamsAll): Rendering => ({
-    template: './src/templates/laravel/resource',
-    outputPath: `Http/Resources`,
-    outputFile: `${uCamelCase(singular(params.tableMaster))}Resource.php`,
-    params
-  }),
-
-  route: (params: ParamsAll): Rendering => ({
-    template: './src/templates/laravel/route',
-    outputPath: `Routes`,
-    outputFile: `api${uCamelCase(singular(params.tableMaster))}.php`,
-    params
-  }),
-
-  routeDetail: (params: ParamsAll): Rendering => ({
+  detailRoute: (params: ParamsAll): Rendering => ({
     template: './src/templates/laravel/routeDetail',
     outputPath: `Routes`,
     outputFile: `api${uCamelCase(singular(params.tableDetailCurrent!.tableName))}.php`,
     params
   }),
 
-  serviceIndex: (params: ParamsAll): Rendering => ({
-    template: './src/templates/laravel/serviceIndex',
-    outputPath: `Http/Services/${uCamelCase(singular(params.tableMaster))}`,
-    outputFile: `Index${uCamelCase(singular(params.tableMaster))}Service.php`,
-    params
-  }),
-
-  serviceStore: (params: ParamsAll): Rendering => ({
-    template: './src/templates/laravel/serviceStore',
-    outputPath: `Http/Services/${uCamelCase(singular(params.tableMaster))}`,
-    outputFile: `Store${uCamelCase(singular(params.tableMaster))}Service.php`,
-    params
-  }),
-
-  serviceUpdate: (params: ParamsAll): Rendering => ({
-    template: './src/templates/laravel/serviceUpdate',
-    outputPath: `Http/Services/${uCamelCase(singular(params.tableMaster))}`,
-    outputFile: `Update${uCamelCase(singular(params.tableMaster))}Service.php`,
-    params
-  }),
-
-  storeDetailRequest: (params: ParamsAll): Rendering => ({
+  detailStoreRequest: (params: ParamsAll): Rendering => ({
     template: './src/templates/laravel/storeDetailRequest',
     outputPath: `Http/Requests/${uCamelCase(singular(params.tableDetailCurrent!.tableName))}`,
     outputFile: `Store${uCamelCase(singular(params.tableDetailCurrent!.tableName))}Request.php`,
     params
   }),
 
-  storeDetailService: (params: ParamsAll): Rendering => ({
+  detailStoreService: (params: ParamsAll): Rendering => ({
     template: './src/templates/laravel/storeDetailService',
     outputPath: `Http/Services/${uCamelCase(singular(params.tableDetailCurrent!.tableName))}`,
     outputFile: `Store${uCamelCase(singular(params.tableDetailCurrent!.tableName))}Service.php`,
     params
   }),
 
-  updateDetailRequest: (params: ParamsAll): Rendering => ({
+  detailUpdateRequest: (params: ParamsAll): Rendering => ({
     template: './src/templates/laravel/updateDetailRequest',
     outputPath: `Http/Requests/${uCamelCase(singular(params.tableDetailCurrent!.tableName))}`,
     outputFile: `Update${uCamelCase(singular(params.tableDetailCurrent!.tableName))}Request.php`,
     params
   }),
 
-  updateDetailService: (params: ParamsAll): Rendering => ({
+  detailUpdateService: (params: ParamsAll): Rendering => ({
     template: './src/templates/laravel/updateDetailService',
     outputPath: `Http/Services/${uCamelCase(singular(params.tableDetailCurrent!.tableName))}`,
     outputFile: `Update${uCamelCase(singular(params.tableDetailCurrent!.tableName))}Service.php`,
     params
-  })
-  
+  }),
+
+  masterController: (params: ParamsAll): Rendering => ({
+    template: './src/templates/laravel/controller',
+    outputPath: `Http/Controllers`,
+    outputFile: `${uCamelCase(singular(params.tableMaster))}Controller.php`,
+    params
+  }),
+
+  masterModel: (params: ParamsAll): Rendering => ({
+    template: './src/templates/laravel/model',
+    outputPath: `Entities`,
+    outputFile: `${uCamelCase(singular(params.tableMaster))}.php`,
+    params
+  }),
+
+  masterRequestStore: (params: ParamsAll): Rendering => ({
+    template: './src/templates/laravel/requestStore',
+    outputPath: `Http/Requests/${uCamelCase(singular(params.tableMaster))}`,
+    outputFile: `Store${uCamelCase(singular(params.tableMaster))}Request.php`,
+    params
+  }),
+
+  masterRequestUpdate: (params: ParamsAll): Rendering => ({
+    template: './src/templates/laravel/requestUpdate',
+    outputPath: `Http/Requests/${uCamelCase(singular(params.tableMaster))}`,
+    outputFile: `Update${uCamelCase(singular(params.tableMaster))}Request.php`,
+    params
+  }),
+
+  masterResource: (params: ParamsAll): Rendering => ({
+    template: './src/templates/laravel/resource',
+    outputPath: `Http/Resources`,
+    outputFile: `${uCamelCase(singular(params.tableMaster))}Resource.php`,
+    params
+  }),
+
+  masterRoute: (params: ParamsAll): Rendering => ({
+    template: './src/templates/laravel/route',
+    outputPath: `Routes`,
+    outputFile: `api${uCamelCase(singular(params.tableMaster))}.php`,
+    params
+  }),
+
+  masterServiceIndex: (params: ParamsAll): Rendering => ({
+    template: './src/templates/laravel/serviceIndex',
+    outputPath: `Http/Services/${uCamelCase(singular(params.tableMaster))}`,
+    outputFile: `Index${uCamelCase(singular(params.tableMaster))}Service.php`,
+    params
+  }),
+
+  masterServiceStore: (params: ParamsAll): Rendering => ({
+    template: './src/templates/laravel/serviceStore',
+    outputPath: `Http/Services/${uCamelCase(singular(params.tableMaster))}`,
+    outputFile: `Store${uCamelCase(singular(params.tableMaster))}Service.php`,
+    params
+  }),
+
+  masterServiceUpdate: (params: ParamsAll): Rendering => ({
+    template: './src/templates/laravel/serviceUpdate',
+    outputPath: `Http/Services/${uCamelCase(singular(params.tableMaster))}`,
+    outputFile: `Update${uCamelCase(singular(params.tableMaster))}Service.php`,
+    params
+  })  
 }
--- src/processes/writeFrontend.ts
+++ src/processes/writeFrontend.ts
@@ -1,38 +1,37 @@
 import vue from '@modules/renderings/vue'
 import rendering from '@utils/rendering';
-//import crud from '@config/crud'
 import { pathFrontend as output } from '@config/output'
 import type { ParamsAll, tableDetailOfMasterCustomized } from '@customTypes/utilsRendering';
 
 export default (paramsOmitOutput: Omit<ParamsAll, 'output'>): void => {
   const params: ParamsAll = {...paramsOmitOutput, output}
-  rendering(vue.routes(params))
-  rendering(vue.datagrid(params))
-  rendering(vue.useDatagrid(params))
-  rendering(vue.services(params))
-  rendering(vue.types(params))
   if (params.tableDetailOfMaster.length===0){
-    rendering(vue.createOrEditMaster(params))
-    rendering(vue.formCreateOrEditMaster(params))
-    rendering(vue.useCreateOrEditMaster(params))   
+    rendering(vue.masterCreateOrEdit(params))
+    rendering(vue.masterFormCreateOrEdit(params))
+    rendering(vue.masterUseCreateOrEdit(params))   
   } else {
     rendering(vue.tabs(params))
-    rendering(vue.tabMaster(params))
-    rendering(vue.useTabMaster(params)) // TODO
-    rendering(vue.formMaster(params)) // TODO
-    rendering(vue.useFormMaster(params)) // TODO
+    rendering(vue.masterTab(params))
+    rendering(vue.masterUseTab(params)) // TODO
+    rendering(vue.masterForm(params)) // TODO
+    rendering(vue.masterUseForm(params)) // TODO
     params.tableDetailOfMaster.forEach(function(table){
       const paramsWhitDetail = {
         ...params,
         tableDetailCurrent: (table as unknown as tableDetailOfMasterCustomized)
       }
-      rendering(vue.formDetail(paramsWhitDetail))
-      rendering(vue.tabDetail(paramsWhitDetail))
-      rendering(vue.useTabDetail(paramsWhitDetail))
-      rendering(vue.useFormDetail(paramsWhitDetail))
-      rendering(vue.servicesDetail(paramsWhitDetail))
-      rendering(vue.typesDetail(paramsWhitDetail))
-      rendering(vue.tableDetail(paramsWhitDetail))
+      rendering(vue.detailForm(paramsWhitDetail))
+      rendering(vue.detailTab(paramsWhitDetail))
+      rendering(vue.detailUseTab(paramsWhitDetail))
+      rendering(vue.detailUseForm(paramsWhitDetail))
+      rendering(vue.detailServices(paramsWhitDetail))
+      rendering(vue.detailTypes(paramsWhitDetail))
+      rendering(vue.detailTable(paramsWhitDetail))
     })
   }
+  rendering(vue.masterRoutes(params))
+  rendering(vue.masterDatagrid(params))
+  rendering(vue.masterUseDatagrid(params))
+  rendering(vue.masterServices(params))
+  rendering(vue.masterTypes(params))
 }

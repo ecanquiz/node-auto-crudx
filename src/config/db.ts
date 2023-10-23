c656f7db896f4f29671b46abb9812dce8eceb081
Ernesto Canquiz <caribestic123@gmail.com>
2023-10-23 16:23:50 -0400

    refactor: Remove DB instance from config and fix: Comma separators are missing

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,13 @@
 import dotenv from 'dotenv'
-import { Client } from 'pg'
 
 dotenv.config();
 
-const dbClient = new Client({    
+const dbConfig = {    
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
     database: process.env.DB_DATABASE,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD    
-});
+}
 
-export default dbClient;
+export default dbConfig;
